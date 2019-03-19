f415a9be9a7296654bd97b96980ca7a06985a01e
Paul Li <paul.li@careguide.com>
2019-03-19 15:02:02 -0400

    fix gitignore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,66 @@
 import React from "react";
 import { render } from "react-dom"
+import Pet from "./Pet"
+import pf from "petfinder-client";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      pets: []
+    };
+  }
+
+  componentDidMount() {
+    petfinder.pet
+      .find({ location: "Toronto, ON", output: "full" })
+      .then(data => {
+        let pets;
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+        this.setState({
+          pets: pets
+        });
+      });
+  }
+
   render() {
     return (
       <div>
         <h1>Bugger!</h1>
+        <div className="search">
+          {this.state.pets.map(pet => {
+            let breed;
+            if (Array.isArray(pet.breeds.breed)) {
+              breed = pet.breeds.breed.join(", ");
+            } else {
+              breed = pet.breeds.breed;
+            }
+            return (
+              <Pet
+                animal={pet.animal}
+                key={pet.id}
+                name={pet.name}
+                breed={breed}
+                media={pet.media}
+                location={`${pet.contact.city}, ${pet.contact.state}`}
+              />
+            );
+          })}
+        </div>
       </div>
     );
   }
